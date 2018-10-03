9a3a8dceb30939421772c15ac20bdbac1bd62606
mizunoawa.web <saeki_takatoshi@outlook.jp>
2018-10-03 21:02:37 +0900

    画像一覧

---
+++ MTGTool/Model/Image.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace MTGTool.Model
+{
+    class Image
+    {
+        public BitmapSource Bitmap { get; private set; }
+
+        public Image(BitmapSource bitmap)
+        {
+            Bitmap = bitmap;
+        }
+    }
+}
---
+++ MTGTool/ViewModel/PictureListViewModel.cs
@@ -0,0 +1,34 @@
+﻿using GalaSoft.MvvmLight;
+using MTGTool.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace MTGTool.ViewModel
+{
+    class PictureListViewModel : ViewModelBase
+    {
+        public ObservableCollection<Image> Pictures { get; } = new ObservableCollection<Image>();
+
+        public PictureListViewModel()
+        {
+            System.IO.DirectoryInfo di = new System.IO.DirectoryInfo(@"Image");
+            IEnumerable<System.IO.FileInfo> files =
+                di.EnumerateFiles("*.png", System.IO.SearchOption.AllDirectories);
+
+            //ファイルを列挙する
+            foreach (System.IO.FileInfo f in files)
+            {
+                MemoryStream data = new MemoryStream(File.ReadAllBytes(f.FullName));
+                WriteableBitmap wbmp = new WriteableBitmap(BitmapFrame.Create(data));
+                data.Close();
+                Pictures.Add(new Image(wbmp));
+            }
+        }
+    }
+}
